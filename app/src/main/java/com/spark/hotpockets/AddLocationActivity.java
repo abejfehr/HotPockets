85108f520ad26a88314ffe06d899f0a57739154d
abejfehr <abe.fehr@gmail.com>
2015-06-17 23:02:04 -0400

    OKAY

--- app/src/main/java/com/spark/hotpockets/AddLocationActivity.java
+++ app/src/main/java/com/spark/hotpockets/AddLocationActivity.java
@@ -1,6 +1,8 @@
 package com.spark.hotpockets;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -18,6 +20,9 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.util.List;
+
 public class AddLocationActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     private Button submit;
@@ -32,24 +37,24 @@ public class AddLocationActivity extends ActionBarActivity implements OnMapReady
         dbManager = new DatabaseManager(getApplicationContext());
 
         //Make the 'Submit' button add the location to the database
-        nicknameEditText = (EditText)findViewById(R.id.nickname);
-        submit = (Button)this.findViewById(R.id.submitButton);
+        nicknameEditText = (EditText) findViewById(R.id.nickname);
+        submit = (Button) this.findViewById(R.id.submitButton);
         submit.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
                         String nickname = "";
-                        if(nicknameEditText.getText().toString().matches("")) {
+                        if (nicknameEditText.getText().toString().matches("")) {
                             noNicknameAddress();
-                        }else{
+                        } else {
 
                             //Toast argument
                             Context context = getApplicationContext();
                             int duration = Toast.LENGTH_SHORT;
                             nickname = nicknameEditText.getText().toString();
                             String added = " Added!";
-                            String text =  nickname + added;
+                            String text = nickname + added;
 
                             //Show toast when location has been submitted
                             Toast toast = Toast.makeText(context, text, duration);
@@ -90,7 +95,7 @@ public class AddLocationActivity extends ActionBarActivity implements OnMapReady
         return super.onOptionsItemSelected(item);
     }
 
-    private void noNicknameAddress(){
+    private void noNicknameAddress() {
 
         Log.i(getString(R.string.HOT_POCKETS), "No Nickname Entered");
 
@@ -107,15 +112,27 @@ public class AddLocationActivity extends ActionBarActivity implements OnMapReady
         Log.i(getString(R.string.HOT_POCKETS), "The map fragment is ready for use");
         googleMap.setMyLocationEnabled(true);
 
-        CameraUpdate center=
+        CameraUpdate center =
                 CameraUpdateFactory.newLatLng(new LatLng(HotPocketMain.lat,
                         HotPocketMain.lng));
-        CameraUpdate zoom= CameraUpdateFactory.zoomTo(15);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
 
         Log.i(getString(R.string.HOT_POCKETS), "Your current location according to this app is " + HotPocketMain.lat + ", " + HotPocketMain.lng);
 
-                googleMap.moveCamera(center);
+        googleMap.moveCamera(center);
         googleMap.animateCamera(zoom);
 
+        Geocoder geoCoder = new Geocoder(getApplicationContext());
+        List<Address> matches = null;
+        try {
+            matches = geoCoder.getFromLocation(HotPocketMain.lat, HotPocketMain.lng, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (matches != null) {
+            Address bestMatch = (matches.isEmpty() ? null : matches.get(0));
+            if (bestMatch != null)
+                nicknameEditText.setText(bestMatch.getAddressLine(0));
+        }
     }
 }
